b76485b8b00011daed400c077df338e3d25b0ef7
陈旭10045812 <chen.xu8@zte.com.cn>
2017-08-07 20:36:11 +0800

    add-web-files

---
+++ app/any-badge/web/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import {Component, Renderer2, ViewContainerRef} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  title = 'Jigsaw seed application';
+
+  constructor(public viewContainerRef: ViewContainerRef, public renderer: Renderer2) {
+  }
+
+  gotoTourist() {
+    window.open('https://github.com/rdkmaster/jigsaw/blob/master/docs/tourist/index.md', '_blank');
+  }
+
+  gotoGithub() {
+    window.open('https://github.com/rdkmaster/jigsaw', '_blank');
+  }
+}
---
+++ app/any-badge/web/src/app/app.module.ts
@@ -0,0 +1,39 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {JigsawTimeModule, JigsawButtonModule} from '@rdkmaster/jigsaw';
+
+import {AppComponent} from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, FormsModule, HttpModule, BrowserAnimationsModule,
+    TranslateModule.forRoot(), JigsawTimeModule, JigsawButtonModule
+  ],
+  providers: [TranslateService],
+  bootstrap: [AppComponent],
+  entryComponents: []
+})
+export class AppModule {
+  constructor(translateService: TranslateService) {
+    translateService.setTranslation('zh', {
+      'get-started': '马上开始',
+      'give-star': '给 Jigsaw 点个星星'
+    });
+    translateService.setTranslation('en', {
+      'get-started': 'Get started',
+      'give-star': 'Give us a star on Github.com'
+    });
+
+    const lang: string = translateService.getBrowserLang();
+    translateService.setDefaultLang(lang);
+    translateService.use(lang);
+  }
+}

c262bbabd9e19498fd20a7780c87113a270e1ba6
陈旭10045812 <chen.xu8@zte.com.cn>
2017-08-21 11:06:16 +0800

    buidl-script

--- run-rdk.bat
+++ run-rdk.bat
@@ -1,3 +1,3 @@
 @echo off
 
-java -Xms64m -Xmx256m -Dfile.encoding=UTF-8 -classpath proc/bin/lib/* com.zte.vmax.rdk.Run
+"%JAVA_HOME%"\bin\java -Xms64m -Xmx256m -Dfile.encoding=UTF-8 -classpath proc/bin/lib/* com.zte.vmax.rdk.Run
